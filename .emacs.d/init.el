abe67f92dcb9d21849c22308ab7c085390cb20de
Andrew Hunter <ahh@rhody.(none)>
2010-04-19 16:10:05 -0700

    asdf

---
+++ .emacs.d/init.el
@@ -0,0 +1,50 @@
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'c-mode-common-hook 'turn-on-font-lock)
+(global-font-lock-mode 1)
+(setq require-final-newline "ask")
+(setq inhibit-startup-screen t)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+(setq c-basic-offset 4)
+(setq c-default-style "k&r")
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(defun indent-or-complete ()
+      "Complete if point is at end of a word, otherwise indent line."
+      (interactive)
+      (if (looking-at "\\>")
+          (dabbrev-expand nil)
+        (indent-for-tab-command)
+        ))
+
+(add-hook 'find-file-hooks (function (lambda ()
+ (local-set-key "\t" 'indent-or-complete))))
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+(defalias 'qrr 'query-replace-regexp)
+(global-set-key "\M-/" 'hippie-expand)
+
+(set-face-bold-p 'font-lock-keyword-face t)
+(set-face-italic-p 'font-lock-comment-face t)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
