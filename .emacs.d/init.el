beafba952ed5713cbf4f2454ba8c887502848747
Andrew Hunter <ahh@cs.washington.edu>
2010-04-28 13:59:16 -0700

    asdf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,20 +36,9 @@
 (setq uniquify-buffer-name-style 'forward)
 
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
-
 (setq load-path (cons "~/.emacs.d/my-elisp" load-path))
+
+(load-file "~/.emacs.d/local.el")
 
 
 (add-to-list 'auto-mode-alist '("\\.cu$" . cuda-mode))
